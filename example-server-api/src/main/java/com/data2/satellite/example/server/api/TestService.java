016a7cfeb20b3a26a6506f12bbba447d28e9fa87
wanglei.asiainfo <wanglei15@asiainfo.com>
2021-04-08 13:01:10 +0800

    增加多个接口测试

--- example-server-api/src/main/java/com/data2/satellite/example/server/api/TestService.java
+++ example-server-api/src/main/java/com/data2/satellite/example/server/api/TestService.java
@@ -3,9 +3,8 @@ package com.data2.satellite.example.server.api;
 /**
  * @author data2
  * @description
- * @date 2021/4/1 下午5:16
+ * @date 2021/4/7 下午8:21
  */
-public interface HiService {
-
-    String hi();
+public interface TestService {
+    void test();
 }
